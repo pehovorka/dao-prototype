9e43393b0d426682ef1a3bc0dcbd4d195e41ceeb
Petr Hovorka <pehovorka@gmail.com>
2023-02-25 13:58:14 +0100

    ✨ Add ability to connect wallet

--- web/src/components/layout/Navbar.tsx
+++ web/src/components/layout/Navbar.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import type { ReactNode } from "react";
+import { FormattedMessage } from "react-intl";
 
 import { NavLink } from "./NavLink";
-import { Languages } from "@/hooks/useLang";
-import { FormattedMessage } from "react-intl";
+import { NavProfileDropdown } from "../profile/NavProfileDropdown";
 
 interface NavbarProps {
   children: ReactNode;
@@ -30,14 +28,6 @@ const menuItems: ReactNode[] = [
 ];
 
 export const Navbar = ({ children }: NavbarProps) => {
-  const router = useRouter();
-  const { locale, pathname, asPath, query } = router;
-
-  const handleLanguageChange = () => {
-    const newLocale = locale === Languages.EN ? Languages.CS : Languages.EN;
-    router.push({ pathname, query }, asPath, { locale: newLocale });
-  };
-
   return (
     <nav className="drawer">
       <input id="side-menu" type="checkbox" className="drawer-toggle" />
@@ -70,28 +60,7 @@ export const Navbar = ({ children }: NavbarProps) => {
             <ul className="menu menu-horizontal px-1">{menuItems}</ul>
           </div>
           <div className="navbar-end">
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <Image
-                    src="/images/user-placeholder.png"
-                    width={40}
-                    height={40}
-                    alt=""
-                  />
-                </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a onClick={handleLanguageChange}>
-                    {locale === Languages.EN ? "Česky" : "English"}
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <NavProfileDropdown />
           </div>
         </div>
         {children}
---
+++ web/src/components/profile/NavProfileDropdown.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEthers } from "@usedapp/core";
+import { FormattedMessage } from "react-intl";
+
+import { Languages } from "@/hooks/useLang";
+
+type LanguageKey = (typeof Languages)[keyof typeof Languages];
+
+export const NavProfileDropdown = () => {
+  const router = useRouter();
+  const { locale, pathname, asPath, query } = router;
+  const { account, deactivate, activateBrowserWallet } = useEthers();
+
+  const handleLanguageSelect = (value: LanguageKey) => {
+    router.push({ pathname, query }, asPath, { locale: value });
+  };
+
+  return (
+    <div className="dropdown dropdown-end">
+      <label
+        tabIndex={0}
+        className="btn btn-ghost gap-3 px-2 flex-nowrap normal-case"
+      >
+        {account ?? <FormattedMessage id="profile.user.anonymous" />}
+        <div className="w-10 rounded-full">
+          <Image
+            src="/images/user-placeholder.png"
+            width={40}
+            height={40}
+            alt=""
+          />
+        </div>
+      </label>
+      <div
+        tabIndex={0}
+        className="card card-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <ul className="menu">
+          <li>
+            {account ? (
+              <a onClick={() => deactivate()}>
+                <FormattedMessage id="profile.wallet.disconnect" />
+              </a>
+            ) : (
+              <a onClick={() => activateBrowserWallet()}>
+                <FormattedMessage id="profile.wallet.connect" />
+              </a>
+            )}
+          </li>
+        </ul>
+        <select
+          className="select select-md"
+          value={locale}
+          onChange={(e) => handleLanguageSelect(e.target.value as LanguageKey)}
+        >
+          <option value={Languages.CS}>CZ</option>
+          <option value={Languages.EN}>EN</option>
+        </select>
+      </div>
+    </div>
+  );
+};
