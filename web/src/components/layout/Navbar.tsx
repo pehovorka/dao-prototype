3f394e7633f209ec5a2151a188711ced609cbb69
Petr Hovorka <pehovorka@gmail.com>
2023-02-26 13:45:40 +0100

    📱 Fix scrolling to footer in mobile Safari

--- web/src/components/layout/Navbar.tsx
+++ web/src/components/layout/Navbar.tsx
@@ -31,7 +31,7 @@ export const Navbar = ({ children }: NavbarProps) => {
   return (
     <nav className="drawer">
       <input id="side-menu" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col h-screen justify-between">
+      <div className="drawer-content flex flex-col h-full justify-between">
         <div className="navbar">
           <div className="navbar-start">
             <div className="flex-none lg:hidden">
