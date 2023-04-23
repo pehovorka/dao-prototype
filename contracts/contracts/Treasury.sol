e03e9d2ee46ebfeff8160b61f98e573e7cde83d7
Petr Hovorka <pehovorka@gmail.com>
2023-04-23 17:32:20 +0200

    ✨ Add receiving funds to Treasury contract

--- contracts/contracts/Treasury.sol
+++ contracts/contracts/Treasury.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Treasury is Ownable {
+    // Enables receiving ether in this contract.
+    receive() external payable {}
+
     // Sends funds from the treasury to an address.
     function sendFunds(uint256 _amount, address _recipient) public onlyOwner {
         payable(_recipient).transfer(_amount);
