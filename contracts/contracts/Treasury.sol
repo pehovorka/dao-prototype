37b5071d699a45aec5f61bcd62f2acdd7cfb88a6
Petr Hovorka <pehovorka@gmail.com>
2023-04-23 13:24:39 +0200

    ✨ Add Treasury contract

---
+++ contracts/contracts/Treasury.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Treasury is Ownable {
+    // Sends funds from the treasury to an address.
+    function sendFunds(uint256 _amount, address _recipient) public onlyOwner {
+        payable(_recipient).transfer(_amount);
+    }
+}
