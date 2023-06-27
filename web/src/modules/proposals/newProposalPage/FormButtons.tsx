842404fc01a83c8f78d2467aa8cc0eb2824cb4d4
Petr Hovorka <pehovorka@gmail.com>
2023-06-27 19:13:36 +0200

    ✨ Require confirmation before cancelling form

--- web/src/modules/proposals/newProposalPage/FormButtons.tsx
+++ web/src/modules/proposals/newProposalPage/FormButtons.tsx
@@ -4,6 +4,8 @@ import { useVotingPower } from "../hooks";
 import { formatEther } from "ethers/lib/utils";
 import { config } from "@/config";
 import { InfoIcon } from "@/assets/icons";
+import { useConfirmDialog } from "@/hooks";
+import { Confirm } from "@/modules/ui";
 
 interface FormButtonsProps {
   loading: boolean;
@@ -13,37 +15,53 @@ export const FormButtons = ({ loading }: FormButtonsProps) => {
   const { push } = useRouter();
   const { votingPower } = useVotingPower();
 
+  const {
+    open: openCancelConfirm,
+    isOpen: isOpenCancelConfirm,
+    close: closeCancelConfirm,
+  } = useConfirmDialog();
+
   return (
-    <div className="flex justify-between mt-8 gap-4 items-center flex-wrap">
-      {votingPower !== undefined ? (
-        <div className="flex items-center gap-1.5 text-sm text-base-content">
-          <InfoIcon className="fill-info w-5" />
-          <FormattedMessage
-            id="proposal.new.page.form.votingPower"
-            values={{
-              votingPower: formatNumber(Number(formatEther(votingPower))),
-              token: config.tokenSymbol,
-            }}
-          />
+    <>
+      <div className="flex justify-between mt-8 gap-4 items-center flex-wrap">
+        {votingPower !== undefined ? (
+          <div className="flex items-center gap-1.5 text-sm text-base-content">
+            <InfoIcon className="fill-info w-5" />
+            <FormattedMessage
+              id="proposal.new.page.form.votingPower"
+              values={{
+                votingPower: formatNumber(Number(formatEther(votingPower))),
+                token: config.tokenSymbol,
+              }}
+            />
+          </div>
+        ) : (
+          <div />
+        )}
+        <div className="flex justify-end gap-4">
+          <button
+            onClick={openCancelConfirm}
+            type="button"
+            disabled={loading}
+            className={`btn btn-ghost`}
+          >
+            <FormattedMessage id="proposal.new.page.form.button.cancel" />
+          </button>
+          <button
+            type="submit"
+            className={`btn btn-primary ${loading && "loading"}`}
+          >
+            <FormattedMessage id="proposal.new.page.form.button.create" />
+          </button>
         </div>
-      ) : (
-        <div />
-      )}
-      <div className="flex justify-end gap-4">
-        <button
-          onClick={() => push("/proposals")}
-          disabled={loading}
-          className={`btn btn-ghost`}
-        >
-          <FormattedMessage id="proposal.new.page.form.button.cancel" />
-        </button>
-        <button
-          type="submit"
-          className={`btn btn-primary ${loading && "loading"}`}
-        >
-          <FormattedMessage id="proposal.new.page.form.button.create" />
-        </button>
       </div>
-    </div>
+      <Confirm
+        open={isOpenCancelConfirm}
+        onConfirm={() => push("/proposals")}
+        onCancel={closeCancelConfirm}
+        title="proposal.new.page.form.cancel.confirm.title"
+        text="proposal.new.page.form.cancel.confirm.description"
+      />
+    </>
   );
 };
